afffc15b59ed9a7c0394c68fee65f5c1b5844d30
nttuyen <nttuyen_it@yahoo.com>
2013-02-07 09:28:58 +0700

    commit: add custom field Location

--- administrator/components/com_jnt_hanhphuc/models/fields/location.php
+++ administrator/components/com_jnt_hanhphuc/models/fields/location.php
@@ -15,6 +15,13 @@ JFormHelper::loadFieldClass('list');
  * @subpackage	com_banners
  * @since		1.6
  */
+
+?>
+<script type="text/javascript">
+
+</script>
+<?php
+
 class JFormFieldLocation extends JFormFieldList
 {
 	/**
@@ -25,35 +32,73 @@ class JFormFieldLocation extends JFormFieldList
 	 */
 	protected $type = 'Location';
 
-	/**
+    protected function getInput() {
+        $input = parent::getInput();
+        $script = '';
+        if($this->element['location_parent_field']) {
+            //TODO: #nttuyen JS load ajax
+            $script = '<script type="text/javascript">
+                            jQuery(document).ready(function($){
+                                jQuery("#jform_'.$this->group.'_'.$this->element['location_parent_field'].'").change(function(){
+                                    value = jQuery(this).val();
+                                    jQuery.get(
+                                        "'.JURI::root().'index.php?option=com_jnt_hanhphuc&task=location.getLocations&type='.$this->element['location_type'].'&parent_column='.$this->element['location_parent_column'].'&parent="+value
+                                        , function(data){
+                                            $input = "";
+                                            currentValue = '.$this->value.';
+                                            jQuery(data).each(function(index, element){
+                                                $input += "<option value=\""+element.value+"\"";
+                                                if(element.value == currentValue) {
+                                                    $input += "selected=\"selected\"";
+                                                }
+                                                $input += ">";
+                                                $input += element.text;
+                                                $input += "</option>";
+                                            });
+                                            jQuery("#jform_'.$this->group.'_'.$this->element['id'].'").html($input);
+                                        }
+                                        , "json"
+                                    );
+                                });
+                            });
+                       </script>';
+        }
+        if($script) {
+            $input .= $script;
+        }
+        //var_dump($input);
+        return $input;
+    }
+
+
+    /**
 	 * Method to get the field options.
 	 *
 	 * @return	array	The field option objects.
 	 * @since	1.6
 	 */
-	public function getOptions()
-	{
-		
-		// Initialize variables.
-		$options = array();
+	public function getOptions() {
 
 		$db		= JFactory::getDbo();
 		$query	= $db->getQuery(true);
-		
-		$get = $this->element['get'];
 
-		if ($get == 'province')
-		{
-			$query->select('id As value, name As text');
-			$query->from('#__banner_clients AS a');
-			$query->order('a.name');
-		}
-		else
-		{
-			$query->select('id As value, name As text');
-			$query->from('#__banner_clients AS a');
-			$query->order('a.name');
-		}
+        $table = '#__location';
+        if($this->element['location_type'] == 'district') {
+            $table .= '_district';
+        } else {
+            $table .= '_province';
+        }
+
+        $query->select('l.id as value, l.title as text');
+        $query->from($table . ' as l');
+        $query->order('l.title');
+
+        if($this->element['location_parent_field'] && $this->element['location_parent_column']) {
+            $group = $this->element['location_parent_group'] ? $this->element['location_parent_group'] : $this->group;
+            $parentField = $this->form->getField($this->element['location_parent_field'], $group);
+            $parentValue = $parentField->value;
+            $query->where('l.'.$this->element['location_parent_column']. ' = '.$parentValue);
+        }
 
 		// Get the options.
 		$db->setQuery($query);
@@ -68,7 +113,7 @@ class JFormFieldLocation extends JFormFieldList
 		// Merge any additional options in the XML definition.
 		//$options = array_merge(parent::getOptions(), $options);
 
-		array_unshift($options, JHtml::_('select.option', '0', JText::_('COM_BANNERS_NO_CLIENT')));
+		array_unshift($options, JHtml::_('select.option', '0', JText::_('COM_JNT_HANHPHUC_NO_LOCATION')));
 
 		return $options;
 	}
