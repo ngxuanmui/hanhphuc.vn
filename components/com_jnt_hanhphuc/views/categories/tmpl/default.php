2a5000fdf8e73fbc1c75c1890a10a53780d40f21
Nguyen Xuan Mui <nguyenxuanmui@gmail.com>
2013-04-23 17:03:24 +0700

    Update view categories & category in service component

--- components/com_jnt_hanhphuc/views/categories/tmpl/default.php
+++ components/com_jnt_hanhphuc/views/categories/tmpl/default.php
@@ -6,21 +6,98 @@ defined('_JEXEC') or die;
 $categories = $this->categories;
 ?>
 
-<h1>Dịch vụ cưới</h1>
+<div class="container">
+    <div class="float-left left-side">
+		<div class="sub-container">
+			<p class="search-text">TÌM KIẾM DỊCH VỤ CƯỚI</p>
 
-<div>
-	<ul>
-		<?php foreach ($categories as $cat): ?>
-		<li>
-			<div><?php echo $cat->title; ?></div>
-			<ul>
-				<?php foreach ($cat->users as $user): ?>
+			<div class="line-break-search"><span></span></div>
+
+			<form id="frm-search-service">
+			<p>Chú ý: <span>Chọn một trong các lựa chọn bên dưới rồi nhấn vào Tìm kiếm để tìm kiếm dịch vụ.</span></p>
+			<div>
+				<select>
+					<option>Chọn dịch vụ</option>
+				</select>
+
+				<select>
+					<option>Tỉnh / Thành</option>
+				</select>
+
+				<select>
+					<option>Quận / Huyện</option>
+				</select>
+			</div>
+
+			<div>
+				<input type="text" placeholder="Gõ tên nhà cung cấp dịch vụ">
+				<button type="button">Tìm kiếm dịch vụ</button>
+			</div>
+
+			</form>
+		</div>
+		
+		<div class="sub-container">
+			<div>
+				<ul class="list-service-categories">
+					<?php foreach ($categories as $cat): ?>
+					<li>
+						<h1 class="category-title"><?php echo $cat->title; ?></h1>
+						<div class="line-break-news"><span></span></div>
+						<ul class="list-service-items fltlft">
+							<?php foreach ($cat->users as $user): ?>
+							<li class="fltlft">
+								<div class="image fltlft">
+									Avatar
+								</div>
+								<div class="fltlft info">
+									<a class="title" href="<?php echo $user->username; ?>">
+										<?php echo $user->name; ?>
+									</a>
+									<p>Địa chỉ: </p>
+									<p>Số điện thoại: </p>
+								</div>
+								<div class="clr"></div>
+							</li>
+							<?php endforeach; ?>
+						</ul>
+						<div class="clr"></div>
+					</li>
+					<?php endforeach; ?>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="float-right right-side">
+		<?php echo JEUtil::loadModule('right', 'module-padding'); ?>
+		
+		<div class="module-title module-padding">THÔNG TIN KHUYẾN MẠI</div>
+		<div class="line-break-promotion"><span></span></div>
+		<div class="box">
+			<ul class="news-other-list">
 				<li>
-					<?php echo $user->username; ?>
+					Áo cưới: ....
 				</li>
-				<?php endforeach; ?>
 			</ul>
-		</li>
-		<?php endforeach; ?>
-	</ul>
+		</div>
+		
+		<div class="module-title module-padding">DOANH NGHIỆP TIÊU BIỂU</div>
+		<div class="line-break"></div>
+		<div class="box">
+			<ul>
+				<li>
+					<div class="img">
+						img here
+					</div>
+					<div class="bussiness-focus-info">
+						<p class="title">Áo cưới</p>
+						<p class="address">Địa chỉ</p>
+						<p class="phone">Điện thoại</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+    </div>
 </div>
+
+<div class="clr"></div>
