393d17a4363623d11561a01e94e50cdb8241779d
Nguyen Xuan Mui <nguyenxuanmui@gmail.com>
2013-06-04 16:07:19 +0700

    Update view service

--- components/com_jnt_hanhphuc/views/service/tmpl/default.php
+++ components/com_jnt_hanhphuc/views/service/tmpl/default.php
@@ -21,78 +21,144 @@ $paymentTypeName = array(
 	//3 => 'Thanh toán qua website nganluong'
 );
 ?>
-<div class="business-intro">
-    <h2><?php echo $businessInfo->profile->business_name ?></h2>
-    <h3><?php echo $serviceInfo->name?></h3>
-    <div class="business-intro-desc">
-    	<?php echo $serviceInfo->description?>
-    </div>
-    <div>
-    	<?php $images = json_decode($serviceInfo->image);
-    	if($images):?>
-    		<ul>
-    		<?php foreach ($images as $image):?>
-    			<li>
-    				<img width="100px" height="90px" alt="" src="<?php echo JURI::base()?>images/users/<?php echo $serviceInfo->business_id?>/services/<?php echo $serviceInfo->id?>/<?php echo $image?>">
-    			</li>
-    		<?php endforeach;?>
-    		</ul>
-    	<?php endif;?>
-    </div>
-    <div>
-    	Thông tin giá và phương thức thanh toán
-    	<ul>
-    		<li>
-    			Giá/chi phí: <?php echo $serviceInfo->price?>
+
+
+<div class="container view-service">
+    <div class="float-left left-side view-service-left">
+    	<h1><?php echo $businessInfo->profile->business_name ?></h1>
+    	
+    	<div class="sub-container relative">
+    		<div class="fltlft images">
+		    	<?php $images = json_decode($serviceInfo->image);
+		    	if($images):?>
+		    		<ul>
+		    		<?php foreach ($images as $image):?>
+		    			<li>
+		    				<img width="100px" height="90px" alt="" src="<?php echo JURI::base()?>images/users/<?php echo $serviceInfo->business_id?>/services/<?php echo $serviceInfo->id?>/<?php echo $image?>">
+		    			</li>
+		    		<?php endforeach;?>
+		    		</ul>
+		    	<?php endif;?>
+		    </div>
+		    
+		    <div class="fltrgt product-info">
+		    	<h3><?php echo $serviceInfo->name?></h3>
+		    	
+		    	<ul class="fltlft">
+		    		<li>
+		    			<label>Người đăng</label>
+		    			<span></span>
+		    		</li>
+		    		<li>
+		    			<label>Danh mục</label>
+		    			<span></span>
+		    		</li>
+		    		<li>
+		    			<label>Giá cũ</label>
+		    			<span><?php echo $serviceInfo->price?></span>
+		    		</li>
+		    		<li>
+		    			<label>Giá mới</label>
+		    			<span><?php echo $serviceInfo->current_price?></span>
+		    		</li>
+		    		<li>
+		    			<label>Khuyến mại</label>
+		    			<span><?php echo empty($serviceInfo->promotion) ? 'Không' : $serviceInfo->promotion?></span>
+		    		</li>
+		    		<li>
+		    			<label>Hình thức thanh toán</label>
+		    			<div>
+		    				<?php $paymentTypes = json_decode($serviceInfo->payment_type);
+			    			if($paymentTypes):
+			    			?>
+			    			<ul>
+			    			<?php foreach ($paymentTypes as $type):?>
+			    				<li><?php echo $paymentTypeName[$type]?></li>
+			    			<?php endforeach;?>
+			    			</ul>
+			    			<?php else:?>
+			    			<div>Không có</div>
+			    			<?php endif;?>
+		    			</div>
+		    		</li>
+		    	</ul>
+		    
+		    <form id="add-service-to-cart" name="add-service-to-cart" action="<?php echo JRoute::_('index.php?option=com_jnt_hanhphuc&task=cart.add') ?>" method="post">
+	            <input type="hidden" name="option" value="com_jnt_hanhphuc"/>
+	            <input type="hidden" name="task" value="cart.add"/>
+	            <input type="hidden" name="id" value="<?php echo $serviceInfo->id ?>"/>
+	            <input type="hidden" name="qty" value="1" size="5"/>
+	            <input type="submit" value="Thêm vào giỏ hàng"/>
+	        </form>
+		    	
+		    	
+		    </div>
+		    
+		    <div class="clr"></div>
+		    
+    	</div>
+    	
+    	<ul class="tabs">
+    		<li rel="tab-1">Thông tin sản phẩm</li>
+    		<li rel="tab-2">Thông tin liên hệ</li>
+    		<li rel="tab-3">Bản đồ</li>
+    	</ul>
+    	
+    	<ul class="tab-content">
+    		<li rel="tab-content-1">
+    			<?php echo $serviceInfo->description?>
     		</li>
-    		<li>
-    			Giá hiện tại: <?php echo $serviceInfo->current_price?>
-    		</li>
-    		<li>
-    			Khuyến mại: <?php echo empty($serviceInfo->promotion) ? 'Không' : $serviceInfo->promotion?>
-    		</li>
-    		<li>
-    			Hình thức thanh toán:
-    			<?php $paymentTypes = json_decode($serviceInfo->payment_type);
-    			if($paymentTypes):
-    			?>
+    		<li rel="tab-content-2">
     			<ul>
-    			<?php foreach ($paymentTypes as $type):?>
-    				<li><?php echo $paymentTypeName[$type]?></li>
-    			<?php endforeach;?>
-    			</ul>
-    			<?php else:?>
-    			<div>Không có</div>
-    			<?php endif;?>
+					<li>
+						Địa chỉ: <?php echo $businessInfo->profile->business_address?>
+						<br/>
+						<?php echo $businessInfo->profile->business_village?> - <?php echo $businessInfo->profile->business_district?> - <?php echo $businessInfo->profile->business_city?>
+					</li>
+					<li>
+						Email: <?php echo $businessInfo->email?>
+					</li>
+					<li>
+						Điện thoại: <?php echo $businessInfo->profile->business_phone?>
+					</li>
+				</ul>
+    		</li>
+    		<li rel="tab-content-3">
+    			Bản đồ đang được cập nhật
     		</li>
     	</ul>
+    	
     </div>
-    <div>
-    	Thông tin liên hệ
-    	<ul>
-			<li>
-				Địa chỉ: <?php echo $businessInfo->profile->business_address?>
-				<br/>
-				<?php echo $businessInfo->profile->business_village?> - <?php echo $businessInfo->profile->business_district?> - <?php echo $businessInfo->profile->business_city?>
-			</li>
-			<li>
-				Email: <?php echo $businessInfo->email?>
-			</li>
-			<li>
-				Điện thoại: <?php echo $businessInfo->profile->business_phone?>
-			</li>
-		</ul>
-    </div>
-    <div>
-        <form id="add-service-to-cart" name="add-service-to-cart" action="<?php echo JRoute::_('index.php?option=com_jnt_hanhphuc&task=cart.add') ?>" method="post">
-            <input type="hidden" name="option" value="com_jnt_hanhphuc"/>
-            <input type="hidden" name="task" value="cart.add"/>
-            <input type="hidden" name="id" value="<?php echo $serviceInfo->id ?>"/>
-            
-            <h4>Add to cart</h4>
-            <label for="qty">Số lượng</label>
-            <input type="text" name="qty" value="1" size="5"/>
-            <input type="submit" value="Add"/>
-        </form>
+    <div class="float-right right-side">
+		<?php echo JEUtil::loadModule('right', 'module-padding'); ?>
+		
+		<div class="module-title module-padding">THÔNG TIN KHUYẾN MẠI</div>
+		<div class="line-break-promotion"><span></span></div>
+		<div class="box">
+			<ul class="news-other-list">
+				<li>
+					Áo cưới: ....
+				</li>
+			</ul>
+		</div>
+		
+		<div class="module-title module-padding">DOANH NGHIỆP TIÊU BIỂU</div>
+		<div class="line-break"></div>
+		<div class="box">
+			<ul>
+				<li>
+					<div class="img">
+						img here
+					</div>
+					<div class="bussiness-focus-info">
+						<p class="title">Áo cưới</p>
+						<p class="address">Địa chỉ</p>
+						<p class="phone">Điện thoại</p>
+					</div>
+				</li>
+			</ul>
+		</div>
     </div>
 </div>
+
+<div class="clr"></div>
