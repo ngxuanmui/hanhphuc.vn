851f9eec75c7b89e7a5ce12d6c6fa81f6271ca60
Nguyen Xuan Mui <nguyenxuanmui@gmail.com>
2013-10-14 17:16:35 +0700

    Update hanhphuc.vn

---
+++ components/com_jnt_hanhphuc/views/articles/view.html.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * HTML Article View class for the Content component
+ *
+ * @package		Joomla.Site
+ * @subpackage	com_content
+ * @since		1.5
+ */
+class Jnt_HanhphucViewArticle extends JViewLegacy
+{
+	protected $item;
+
+	function display($tpl = null)
+	{
+		// Initialise variables.
+		$this->item		= $this->get('Item');
+
+		$this->_prepareDocument();
+
+		parent::display($tpl);
+	}
+
+	/**
+	 * Prepares the document
+	 */
+	protected function _prepareDocument()
+	{
+		$app	= JFactory::getApplication();
+		$menus	= $app->getMenu();
+		$pathway = $app->getPathway();
+		$title = null;
+
+	}
+}
