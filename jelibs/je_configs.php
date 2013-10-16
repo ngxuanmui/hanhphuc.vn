d22f7ec2f2a5128608072a4b10a71627f95ee3ee
Nguyen Xuan Mui <nguyenxuanmui@gmail.com>
2013-10-16 16:38:35 +0700

    Update user content

--- jelibs/je_configs.php
+++ jelibs/je_configs.php
@@ -9,3 +9,5 @@ define('CFG_UPLOAD_IMAGES_PATH', JPATH_ROOT . DS . 'images');
 /* for localhost */
 define('CFG_RECAPTCHA_PUBLIC_KEY', '6LdvOMsSAAAAANA--SXCJv4ZolckztwqFueHq3wn');
 define('CFG_RECAPTCHA_PRIVATE_KEY', '6LdvOMsSAAAAAPSPT9vfxAQttUaxzGB7fs1NyV7C');
+
+define('CFG_LIST_USER_CONTENT', 20);
