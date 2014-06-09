9a85ae32eabc54aa28aceb72c059da4c3d5c8bb6
Nguyen Xuan Mui <nguyenxuanmui@gmail.com>
2014-06-09 16:23:13 +0700

    hp.vn: Update mod b blogs

--- modules/mod_hp_b_blogs/helper.php
+++ modules/mod_hp_b_blogs/helper.php
@@ -10,12 +10,13 @@ defined('_JEXEC') or die;
 
 class modBusinessBlogHelper {
 	static function &getListBlogs(&$params) {
-        $limit = $params->get('limit');
+        $limit = 24; // $params->get('limit');
         $db = JFactory::getDbo();
         $query = $db->getQuery(true);
         $query->select('p.*')
             ->from('#__hp_business_profile p')
             ->innerJoin('#__users u ON p.user_id = u.id')
+            ->where('block != 1')
             ->where('u.user_type = 1')
             ->order('u.registerDate desc');
         //var_dump(str_replace('#__', 'hp_', $query->__toString()));
